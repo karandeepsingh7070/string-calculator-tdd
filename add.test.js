4bd131ca3cbef2b19c4458da7d5797cb775fab3d
karandeepsingh7070 <soundportrait2530@gmail.com>
2025-03-19 11:31:55 +0530

    step 1 test cases

--- add.test.js
+++ add.test.js
@@ -5,4 +5,12 @@ describe('String Calculator - Step 1',() => {
     it('should return 0 for an empty string', () => {
         expect(Add("")).toBe(0);
     });
+
+    it('should return the same number when only one number is passed', () => {
+        expect(Add("1")).toBe(1);
+    });
+
+    it('should return the sum of two numbers separated by a comma', () => {
+        expect(Add("7,9")).toBe(16);
+    });
 })
